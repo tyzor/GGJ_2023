a1005875991de06f5c33145a6266648cbab61b48
CieroCeramics <red_wolf19@hotmail.com>
2023-02-02 18:06:39 -0800

    made an attack sort of just a little bit tho
    it really is only a little bit

--- Assets/Scripts/Player/PlayerAttackController.cs
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -5,6 +5,10 @@ namespace GGJ.Player
 {
     public class PlayerAttackController : MonoBehaviour
     {
+        public float Maxintensity = 10;
+        
+        private float startTime =0;
+        private float endTime = 0;
         private void Start()
         {
             InputDelegator.OnAttackPressed += OnAttackPressed;
@@ -12,22 +16,43 @@ namespace GGJ.Player
 
         private void OnAttackPressed(bool isPressed)
         {
-            float intensity = 0;
+
             if (isPressed)
             {
-                intensity +=0.1f;
-                if (intensity >10)
-                {
-                    intensity = 10;
-                }
+                startTime= Time.time;
+                // intensity +=0.1f;
+                // if (intensity >Maxintensity)
+                // {
+                //     intensity = Maxintensity;
+                // }
+                //Debug.Log(intensity);
             }
             else
             {
+                endTime =  Time.time - startTime;
+                Debug.Log(endTime);
+
+                switch (endTime)
+                {
+                    case float i when i > 0 && i< 0.5f:
+                        Debug.Log(0);
+                        break;
+                    case float i when i > 0.5 && i< 1.0f:
+                        Debug.Log(1);
+                        break;
+                    case float i when i > 1.0 && i< 1.5f:
+                        Debug.Log(2);
+                        break;
+                    case float i when i > 1.5 && i< 2.0f:
+                        Debug.Log(3);
+                        break;
+                }
+                //intensity =0;
                 //Determine how long we were pressing 
                 //Do appropriate attack
             }
 
-            Debug.Log(intensity);
+            
         }
     }
 }
