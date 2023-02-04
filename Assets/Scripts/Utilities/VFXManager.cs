dd6166827ccfc68474ddbc6c87b9fe4cf5545711
Alex Bedard-Reid <alexbedardreid@gmail.com>
2023-02-03 16:21:17 -0800

    Added VFX Manager

---
+++ Assets/Scripts/Utilities/VFXManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.Utilities
+{
+    public enum VFX
+    {
+        NONE,
+        HIT_EFFECT,
+        SPIN_EFFECT,
+        
+    }
+    
+    public class VFXManager : MonoBehaviour
+    {
+        //============================================================================================================//
+        [Serializable]
+        public struct VFXData
+        {
+            public string name;
+            public VFX type;
+            public GameObject prefab;
+            public float lifetime;
+        }
+
+        //============================================================================================================//
+        
+        private static VFXManager _instance;
+
+        public static GameObject CreateVFX(VFX vfx, Vector3 worldPosition)
+        {
+            return _instance.TryCreateVFX(vfx, worldPosition);
+        }
+
+        //============================================================================================================//
+        
+        [SerializeField]
+        private VFXData[] vfx;
+
+        private Dictionary<VFX, VFXData> _vfxDatas;
+
+        //Unity Functions
+        //============================================================================================================//
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+
+        private void Start()
+        {
+            foreach (var vfxData in vfx)
+            {
+                //todo fill dictionary
+            }
+        }
+        //============================================================================================================//
+        
+        private GameObject TryCreateVFX(VFX vfx, Vector3 worldPosition)
+        {
+            throw new NotImplementedException();
+        }
+        //============================================================================================================//
+    }
+}
