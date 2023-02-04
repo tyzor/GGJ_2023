027db0925e92a2f2621ade1ae2a35ac07f046140
Alex Bedard-Reid <alexbedardreid@gmail.com>
2023-02-03 16:08:51 -0800

    WIP Collectables

---
+++ Assets/Scripts/CollectableBase.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using GGJ.Player;
+using UnityEngine;
+
+namespace GGJ.Collectables
+{
+    public class CollectableBase : MonoBehaviour
+    {
+        private static Transform _playerTransform;
+
+        
+        [SerializeField]
+        private float pickupDistance;
+        [SerializeField]
+        private float drag;
+        [SerializeField]
+        private float accel;
+        
+        private Vector3 velocity;
+        private bool canBePickedUp;
+        private float pickupCountdown;
+
+        private bool launched;
+
+        //Unity Functions
+        //============================================================================================================//
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            if (_playerTransform == null)
+                _playerTransform = FindObjectOfType<PlayerController>().transform;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (launched == false)
+                return;
+            
+            if (canBePickedUp == false)
+            {
+                if (pickupCountdown > 0f)
+                {
+                    pickupCountdown -= Time.deltaTime;
+                }
+                else
+                    canBePickedUp = true;
+
+                //Drag
+                velocity -= velocity * (Time.deltaTime * drag);
+                
+                transform.position += velocity * Time.deltaTime;
+                return;
+            }
+
+            var dirToPlayer = _playerTransform.position - transform.position;
+
+            //Distance check
+            if (dirToPlayer.magnitude > pickupDistance)
+            {
+                velocity -= velocity * (Time.deltaTime * drag);
+                transform.position += velocity * Time.deltaTime;
+                return;
+            }
+
+            velocity += dirToPlayer.normalized * (Time.deltaTime * accel);
+
+            transform.position += velocity * Time.deltaTime;
+        }
+
+        //============================================================================================================//
+
+        public void Launch(Vector3 direction, float speed, float pickupDelay = 1f)
+        {
+            velocity = direction.normalized * speed;
+
+            pickupCountdown = pickupDelay;
+
+            launched = true;
+        }
+
+
+        //============================================================================================================//
+
+        public static void CreateCollectableAt(Vector3 worldPosition)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GGJ.Collectables
+{
+    public class CollectableController : MonoBehaviour
+    {
+        [SerializeField]
+        private CollectableBase collectablePrefab;
+
+        [SerializeField, Min(1)]
+        private int spawnCount;
+        [SerializeField]
+        private Vector3 spawnPosition;
+
+        [SerializeField, Min(0f)]
+        private float launchSpeed;
+        [SerializeField, Min(0f)]
+        private float pickupDelay = 1f;
+
+        [ContextMenu("Spawn Test Collectables")]
+        private void SpawnCollectables()
+        {
+            if (Application.isPlaying == false)
+                return;
+            
+            for (int i = 0; i < spawnCount; i++)
+            {
+                var newCollectable = Instantiate(collectablePrefab, spawnPosition, quaternion.identity, transform);
+
+                var dir = Random.insideUnitCircle.normalized;
+                
+                newCollectable.Launch(new Vector3(dir.x, 0, dir.y), launchSpeed, pickupDelay);
+            }
+        }
+    }
+}
